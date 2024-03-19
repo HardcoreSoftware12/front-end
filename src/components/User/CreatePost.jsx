86f8dda52cdc6c5cb732c40766f2843cf9355239
cipher <devadigaranjith12@gmail.com>
2024-03-19 22:04:39 +0530

    .

--- src/components/User/CreatePost.jsx
+++ src/components/User/CreatePost.jsx
@@ -24,7 +24,9 @@ function CreatePost() {
   const handleSubmit = async(e) => {
     e.preventDefault();
     // Handle form submission logic here
-   const res =  await axios.post("http://localhost:8000/notes/createpost",formData)
+   const res =  await axios.post("http://localhost:8000/notes/createpost",formData,{
+    headers:{'Content-Type':'multipart/form-data'}
+   })
    console.log(res.data);
    setFormData({
     title: '',
@@ -37,7 +39,8 @@ function CreatePost() {
   return (
     <div className="max-w-lg mx-auto mt-8">
     <h1 className="text-2xl font-bold mb-4">Create New Item</h1>
-    <form onSubmit={handleSubmit} className="space-y-6">
+    <form onSubmit={handleSubmit} className="space-y-6" encType='multipart/form-data'>
+      {/* TITLE */}
       <div className="flex items-center mb-4">
         <label htmlFor="title" className="w-1/3 text-right pr-4">Title:</label>
         <input
@@ -51,7 +54,7 @@ function CreatePost() {
       </div>
 
     
-
+      {/* SMALL DESCRIPTION  */}
       <div className="flex items-center mb-4">
         <label htmlFor="smallDescription" className="w-1/3 text-right pr-4">Small Description:</label>
         <input
@@ -64,6 +67,7 @@ function CreatePost() {
         />
       </div>
 
+      {/* FULL BLOG PARAGRAPH  */}
       <div className="flex items-center mb-4">
         <label htmlFor="description" className="w-1/3 text-right pr-4">Description:</label>
         <textarea
@@ -75,6 +79,7 @@ function CreatePost() {
         />
       </div>
 
+      {/* IMAGE FOR BLOG  */}
       <div className="flex items-center mb-4">
         <label htmlFor="photo" className="w-1/3 text-right pr-4">Banner Image:</label>
         <input
@@ -87,6 +92,8 @@ function CreatePost() {
         />
       </div>
 
+
+       {/* CHOOSE CATEGORY  */}
       <div className="flex items-center mb-4">
         <label htmlFor="category" className="w-1/3 text-right pr-4">Category:</label>
         <select
